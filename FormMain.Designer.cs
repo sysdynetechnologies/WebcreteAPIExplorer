32f144763fed4dc3f212b9cb1eac7ff97b899b93
HONGBO LIU <liuhongbo@sysdyne.onmicrosoft.com>
2018-12-11 15:52:22 -0500

    init commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,249 @@
+namespace WebcreteAPIExplorer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.textBoxRequest = new System.Windows.Forms.TextBox();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxRequest = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(103, 18);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(246, 20);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.Text = "http://api.concretego.com/webcreteapi.asmx";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(418, 18);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(134, 20);
+            this.textBoxUserName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(627, 18);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(136, 20);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(358, 45);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(124, 25);
+            this.buttonProcess.TabIndex = 6;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // textBoxRequest
+            // 
+            this.textBoxRequest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRequest.Location = new System.Drawing.Point(3, 23);
+            this.textBoxRequest.Multiline = true;
+            this.textBoxRequest.Name = "textBoxRequest";
+            this.textBoxRequest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRequest.Size = new System.Drawing.Size(480, 493);
+            this.textBoxRequest.TabIndex = 7;
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResponse.Location = new System.Drawing.Point(3, 23);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResponse.Size = new System.Drawing.Size(481, 493);
+            this.textBoxResponse.TabIndex = 8;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(8, 79);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxRequest);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxResponse);
+            this.splitContainer1.Size = new System.Drawing.Size(977, 519);
+            this.splitContainer1.SplitterDistance = 486;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Request";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 7);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Response";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Request Sample";
+            // 
+            // comboBoxRequest
+            // 
+            this.comboBoxRequest.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRequest.FormattingEnabled = true;
+            this.comboBoxRequest.Items.AddRange(new object[] {
+            "VersionQuery",
+            "CompanyQuery",
+            "CustomerQuery",
+            "ItemQuery",
+            "ProjectQuery",
+            "TicketQuery",
+            "CustomerUpdate",
+            "ProjectUpdate",
+            "ItemUpdate"});
+            this.comboBoxRequest.Location = new System.Drawing.Point(103, 48);
+            this.comboBoxRequest.Name = "comboBoxRequest";
+            this.comboBoxRequest.Size = new System.Drawing.Size(246, 21);
+            this.comboBoxRequest.TabIndex = 11;
+            this.comboBoxRequest.SelectedIndexChanged += new System.EventHandler(this.comboBoxRequest_SelectedIndexChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 610);
+            this.Controls.Add(this.comboBoxRequest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "WebcreteAPI Explorer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.TextBox textBoxRequest;
+        private System.Windows.Forms.TextBox textBoxResponse;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxRequest;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WebcreteAPIExplorer.com.concretego.api;
+using System.Security.Cryptography;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WebcreteAPIExplorer
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxResponse.Text = "";
+                WebcreteAPI api = new WebcreteAPI();
+                if (textBoxURL.Text.Length > 0)
+                    api.Url = textBoxURL.Text;
+
+                api.Timeout = -1;
+
+                string publicKey = api.GetPublicKey("Test", "F52D2965BF8318F");
+               
+                if (publicKey == null)
+                {
+                    MessageBox.Show("Can not get public key.");
+                    return;
+                }
+                RSACryptoServiceProvider key = new RSACryptoServiceProvider();
+                key.FromXmlString(publicKey);
+                UnicodeEncoding enc = new UnicodeEncoding();
+
+                byte[] encryptedPassword;
+
+                encryptedPassword = key.Encrypt(enc.GetBytes(textBoxPassword.Text), false);
+
+                if (api.Login(textBoxUserName.Text, encryptedPassword) == false)
+                {
+                    MessageBox.Show("Login failed.");
+                    return;
+                }
+                string response = api.ProcessRequest(textBoxRequest.Text);
+                if (response != null)
+                {
+                    textBoxResponse.Text = IndentXMLString(response);
+                }
+                else
+                {
+                    MessageBox.Show("Request failed.");
+                    return;
+                }
+                api.Logout();
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+     
+        public string GetVersionQueryRequest()
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+               new XDeclaration("1.0", "utf-8", "yes"),
+               new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+               new XElement("WebcreteXML",
+                   new XElement("WebcreteXMLMsgsRq", 
+                       new XElement("VersionQueryRq", ""))));
+
+            return x.ToString();
+        }
+
+        public string GetCompanyQueryRequest()
+        { 
+            XDocument x = new System.Xml.Linq.XDocument(
+               new XDeclaration("1.0", "utf-8", "yes"),
+               new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+               new XElement("WebcreteXML",
+                   new XElement("WebcreteXMLMsgsRq", 
+                       new XElement("CompanyQueryRq", ""))));
+
+            return x.ToString();
+        }
+
+        public string GetCustomerQueryRequest()
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+               new XDeclaration("1.0", "utf-8", "yes"),
+               new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+               new XElement("WebcreteXML",
+                   new XElement("WebcreteXMLMsgsRq", 
+                       new XElement("CustomerQueryRq", ""))));
+
+            return x.ToString();
+        }
+
+        public string GetProjectQueryRequest()
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+               new XDeclaration("1.0", "utf-8", "yes"),
+               new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+               new XElement("WebcreteXML",
+                   new XElement("WebcreteXMLMsgsRq", 
+                       new XElement("ProjectQueryRq", ""))));
+
+            return x.ToString();
+        }
+
+        public string GetItemQueryRequest()
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+              new XDeclaration("1.0", "utf-8", "yes"),
+              new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+              new XElement("WebcreteXML",
+                  new XElement("WebcreteXMLMsgsRq", 
+                      new XElement("ItemQueryRq", ""))));
+
+            return x.ToString();
+        }
+
+        public string GetTicketQueryRequest(DateTime dtOrderFrom, DateTime dtOrderTo)
+        {
+           XDocument x = new System.Xml.Linq.XDocument(
+             new XDeclaration("1.0", "utf-8", "yes"),
+             new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+             new XElement("WebcreteXML",
+                 new XElement("WebcreteXMLMsgsRq",
+                     new XElement("TicketQueryRq", 
+                         new XElement("FromOrderDate",dtOrderFrom.ToShortDateString()),
+                         new XElement("ToOrderDate",dtOrderTo.ToShortDateString())))));
+
+           return x.ToString();
+        }
+
+        public string GetCustomerUpdateRequest(string code, string name, string sort)
+        {          
+            XDocument x = new System.Xml.Linq.XDocument(
+             new XDeclaration("1.0", "utf-8", "yes"),
+             new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+             new XElement("WebcreteXML",
+                 new XElement("WebcreteXMLMsgsRq",
+                     new XElement("CustomerUpdateRq", 
+                         new XElement("CustomerUpdate",
+                            new XElement("Name",name),
+                            new XElement("Code",code),
+                            new XElement("Sort",sort))))));
+
+           return x.ToString();
+        }
+
+        public string GetProjectUpdateRequest(string customerCode, string code, string name)
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+             new XDeclaration("1.0", "utf-8", "yes"),
+             new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+             new XElement("WebcreteXML",
+                 new XElement("WebcreteXMLMsgsRq",
+                     new XElement("ProjectUpdateRq",
+                         new XElement("ProjectUpdate",
+                            new XElement("CustomerCode", customerCode),
+                            new XElement("Name", name),
+                            new XElement("Code", code))))));
+
+            return x.ToString();
+        }
+
+        public string GetItemUpdateRequest(string code, string description, string shortDescription, string categoryCode)
+        {
+            XDocument x = new System.Xml.Linq.XDocument(
+             new XDeclaration("1.0", "utf-8", "yes"),
+             new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+             new XElement("WebcreteXML",
+                 new XElement("WebcreteXMLMsgsRq",
+                     new XElement("ItemUpdateRq",
+                         new XElement("ItemUpdate",
+                            new XElement("Code", code),
+                            new XElement("Description", description),
+                            new XElement("ShortDescription", shortDescription),
+                            new XElement("CategoryCode", categoryCode))))));
+
+            return x.ToString();
+        }
+
+        private void comboBoxRequest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxRequest.SelectedItem.ToString())
+            {
+                case "VersionQuery":
+                    textBoxRequest.Text = IndentXMLString(GetVersionQueryRequest());
+                    break;
+                case "CompanyQuery":
+                    textBoxRequest.Text = IndentXMLString(GetCompanyQueryRequest());
+                    break;
+                case "CustomerQuery":
+                    textBoxRequest.Text = IndentXMLString(GetCustomerQueryRequest());
+                    break;
+                case "TicketQuery":
+                    textBoxRequest.Text = IndentXMLString(GetTicketQueryRequest(DateTime.Now,DateTime.Now));
+                    break;           
+                case "ItemQuery":
+                    textBoxRequest.Text = IndentXMLString(GetItemQueryRequest());
+                    break;
+                case "ProjectQuery":
+                    textBoxRequest.Text = IndentXMLString(GetProjectQueryRequest());
+                    break;
+                case "CustomerUpdate":
+                    textBoxRequest.Text = IndentXMLString(GetCustomerUpdateRequest("ABCConcrete","ABCConcrete Inc.","ABCConcrete"));
+                    break;
+                case "ProjectUpdate":
+                    textBoxRequest.Text = IndentXMLString(GetProjectUpdateRequest("ABCConcrete", "11229", "Walmart"));
+                    break;
+                case "ItemUpdate":
+                    textBoxRequest.Text = IndentXMLString(GetItemUpdateRequest("4011", "4000 psi pump", "4000 psi", "MIX"));
+                    break;
+            }
+        }
+
+      private string IndentXMLString(string xml)
+      {
+         string outXml = string.Empty;
+         MemoryStream ms = new MemoryStream();
+         // Create a XMLTextWriter that will send its output to a memory stream (file)
+         XmlTextWriter xtw = new XmlTextWriter(ms, Encoding.Unicode);
+         XmlDocument doc = new XmlDocument();
+
+         try
+         {
+            // Load the unformatted XML text string into an instance 
+            // of the XML Document Object Model (DOM)
+            doc.LoadXml(xml);
+
+            // Set the formatting property of the XML Text Writer to indented
+            // the text writer is where the indenting will be performed
+            xtw.Formatting = Formatting.Indented;
+
+            // write dom xml to the xmltextwriter
+            doc.WriteContentTo(xtw);
+            // Flush the contents of the text writer
+            // to the memory stream, which is simply a memory file
+            xtw.Flush();
+
+            // set to start of the memory stream (file)
+            ms.Seek(0, SeekOrigin.Begin);
+            // create a reader to read the contents of 
+            // the memory stream (file)
+            StreamReader sr = new StreamReader(ms);
+            // return the formatted string to caller
+            return sr.ReadToEnd();
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.ToString());
+            return string.Empty;
+         }
+      }
+   }        
+}
